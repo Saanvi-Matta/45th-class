9db87cdd24870351ccce1841257dd3212ebb66b2
Saanvi-Matta <148595557+Saanvi-Matta@users.noreply.github.com>
2023-10-20 19:42:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+n=prompt("Enter the number you want for multiplacation table");
+r=prompt("Enter how many rows you would like");
+
+for(i=1;i<=r;i++){
+    document.write(n+"x"+i+"="+n*i);
+    document.write("<br>");
+}
